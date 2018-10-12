4e982b718010b5e66da61c1d104e439b6324da44
aswinthaikkat <aswinthaikkat@.com>
2018-10-12 20:33:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/recycleviewintro/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.user.recycleviewintro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView r;
+    List<model> l;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        r = findViewById(R.id.r);
+        r.setHasFixedSize(true);
+        r.setLayoutManager(new LinearLayoutManager(this));
+        l  = new ArrayList<>();
+        l.add(
+                new model(
+                        1,
+                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.mipmap.ic_launcher
+                ));
+
+        l.add(
+                new model(
+                        1,
+                        "Dell Inspiron 7000 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home)",
+                        "14 inch, Gray, 1.659 kg",
+                        4.3,
+                        60000,
+                        R.mipmap.ic_launcher));
+
+        l.add(
+                new model(
+                        1,
+                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.mipmap.ic_launcher));
+        r.setAdapter(new RecycleViewAdapter(this,l));
+    }
+}
